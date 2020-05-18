9b2681fcfd09f85d88d088b5d71f3798385a9b0b
pierroelmito <pierroelmito@gmail.com>
2020-05-18 16:04:32 +0200

    make in work on my chromebook

--- 2in1screen.c
+++ 2in1screen.c
@@ -4,103 +4,115 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <math.h>
 
-#define DATA_SIZE 256
-#define N_STATE 2
-char basedir[DATA_SIZE];
-char *basedir_end = NULL;
-char content[DATA_SIZE];
-char command[DATA_SIZE*4];
+int compute_state(double x, double y, double z)
+{
+	double ax = abs(x);
+	double az = abs(z);
 
-char *ROT[]   = {"normal", 				"inverted", 			"left", 				"right"};
-char *COOR[]  = {"1 0 0 0 1 0 0 0 1",	"-1 0 1 0 -1 1 0 0 1", 	"0 -1 1 1 0 0 0 0 1", 	"0 1 0 -1 0 1 0 0 1"};
-// char *TOUCH[] = {"enable", 				"disable", 				"disable", 				"disable"};
-
-double accel_y = 0.0,
-#if N_STATE == 4
-	   accel_x = 0.0,
-#endif
-	   accel_g = 7.0;
-
-int current_state = 0;
-
-int rotation_changed(){
 	int state = 0;
+	if (ax < az) {
+		state = z > 0 ? 0 : 1;
+	} else {
+		state = x > 0 ? 3 : 2;
+	}
 
-	if(accel_y < -accel_g) state = 0;
-	else if(accel_y > accel_g) state = 1;
-#if N_STATE == 4
-	else if(accel_x > accel_g) state = 2;
-	else if(accel_x < -accel_g) state = 3;
+	return state;
+}
+
+FILE* bdopen(const char* base, char const *fname, char leave_open){
+	char path[256];
+	snprintf(path, sizeof(path), "%s/%s", base, fname);
+	FILE *fin = fopen(path, "r");
+	if (fin != NULL)
+		setvbuf(fin, NULL, _IONBF, 0);
+	return fin;
+}
+
+void rotate_screen(int state, const char* xdevName)
+{
+	char *ROT[]   = {
+		"normal",
+		"inverted",
+		"left",
+		"right"
+	};
+	char *COOR[]  = {
+		" 1  0  0  0  1  0  0  0  1",
+		"-1  0  1  0 -1  1  0  0  1",
+		" 0 -1  1  1  0  0  0  0  1",
+		" 0  1  0 -1  0  1  0  0  1"
+	};
+#if 0
+	char *TOUCH[] = {
+		"enable",
+		"disable",
+		"disable",
+		"disable"
+	};
 #endif
-
-	if(current_state!=state){
-		current_state = state;
-		return 1;
-	}
-	else return 0;
-}
-
-FILE* bdopen(char const *fname, char leave_open){
-	*basedir_end = '/';
-	strcpy(basedir_end+1, fname);
-	FILE *fin = fopen(basedir, "r");
-	setvbuf(fin, NULL, _IONBF, 0);
-	fgets(content, DATA_SIZE, fin);
-	*basedir_end = '\0';
-	if(leave_open==0){
-		fclose(fin);
-		return NULL;
-	}
-	else return fin;
-}
-
-void rotate_screen(){
-	sprintf(command, "xrandr -o %s", ROT[current_state]);
+	char command[1024];
+	snprintf(command, sizeof(command), "xrandr -o %s", ROT[state]);
 	system(command);
-	sprintf(command, "xinput set-prop \"%s\" \"Coordinate Transformation Matrix\" %s", "Wacom HID 4846 Finger", COOR[current_state]);
+	snprintf(command, sizeof(command), "xinput set-prop \"%s\" \"Coordinate Transformation Matrix\" %s", xdevName, COOR[state]);
 	system(command);
 }
 
-int main(int argc, char const *argv[]) {
+double readFloat(FILE* f)
+{
+	char content[256];
+	fseek(f, 0, SEEK_SET);
+	fgets(content, sizeof(content), f);
+	return atof(content);
+}
+
+int main(int argc, char const *argv[])
+{
 	FILE *pf = popen("ls /sys/bus/iio/devices/iio:device*/in_accel*", "r");
-	if(!pf){
+
+	if(!pf) {
 		fprintf(stderr, "IO Error.\n");
 		return 2;
 	}
 
-	if(fgets(basedir, DATA_SIZE , pf)!=NULL){
-		basedir_end = strrchr(basedir, '/');
-		if(basedir_end) *basedir_end = '\0';
-		fprintf(stderr, "Accelerometer: %s\n", basedir);
-	}
-	else{
+	char basedir[256];
+	if (fgets(basedir, sizeof(basedir), pf) == NULL) {
 		fprintf(stderr, "Unable to find any accelerometer.\n");
 		return 1;
 	}
+
 	pclose(pf);
 
-	bdopen("in_accel_scale", 0);
-	double scale = atof(content);
+	char *basedir_end = strrchr(basedir, '/');
+	if (basedir_end)
+		*basedir_end = '\0';
 
-	FILE *dev_accel_y = bdopen("in_accel_y_raw", 1);
-#if N_STATE == 4
-	FILE *dev_accel_x = bdopen("in_accel_x_raw", 1);
-#endif
+	fprintf(stderr, "Accelerometer: '%s'\n", basedir);
 
-	while(1){
-		fseek(dev_accel_y, 0, SEEK_SET);
-		fgets(content, DATA_SIZE, dev_accel_y);
-		accel_y = atof(content) * scale;
-#if N_STATE == 4
-		fseek(dev_accel_x, 0, SEEK_SET);
-		fgets(content, DATA_SIZE, dev_accel_x);
-		accel_x = atof(content) * scale;
-#endif
-		if(rotation_changed())
-			rotate_screen();
-		sleep(2);
+	FILE* dev_accel_y = bdopen(basedir, "in_accel_y_raw", 1);
+	FILE* dev_accel_x = bdopen(basedir, "in_accel_x_raw", 1);
+	FILE* dev_accel_z = bdopen(basedir, "in_accel_z_raw", 1);
+
+	int lastState = 0;
+
+	while (1) {
+		const double accel_x = readFloat(dev_accel_x);
+		const double accel_y = readFloat(dev_accel_y);
+		const double accel_z = readFloat(dev_accel_z);
+
+		const int state = compute_state(accel_x, accel_y, accel_z);
+		if (state != lastState) {
+			lastState = state;
+			//rotate_screen("Wacom HID 4846 Finger");
+			rotate_screen(state, "Elan Touchscreen");
+		}
+
+		//fprintf(stderr, "r: %d / x: %.03f / y: %.03f / z: %.03f\n", lastState, accel_x, accel_y, accel_z);
+
+		sleep(1);
 	}
-	
+
 	return 0;
 }
+
